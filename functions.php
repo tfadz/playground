622556d0f83af93c60e6bacce495624d9fcc24a7
Terry Fadz <alaterry@gmail.com>
2019-08-20 16:34:42 -0500

    added ajax filtering to blog posts

--- functions.php
+++ functions.php
@@ -166,7 +166,7 @@ function blog_filter_function(){
  
 	if( $query->have_posts() ) :
 		while( $query->have_posts() ): $query->the_post();
-			echo '<h2>' . $query->post->post_title . '</h2>';
+			include get_stylesheet_directory() . '/template-parts/post-card.php';
 		endwhile;
 		wp_reset_postdata();
 	else :
--- index.php
+++ index.php
@@ -18,69 +18,53 @@ get_header();
 <div id="primary" class="content-area">
 	<main id="main" class="site-main container">
 
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="blog-search-filter">
-			
-
-			<form action="<?php echo site_url() ?>/wp-admin/admin-ajax.php" method="POST" id="filter">
-			<?php
-			if( $terms = get_terms( array( 'taxonomy' => 'category', 'orderby' => 'name' ) ) ) : 
-
-			echo '<select name="categoryfilter"><option value="">Select category...</option>';
-			foreach ( $terms as $term ) :
-			echo '<option value="' . $term->term_id . '">' . $term->name . '</option>'; // ID of the category as the value of an option
-			endforeach;
-			echo '</select>';
-			endif;
-			?>
-
-			
-			<label>
-			<input type="radio" name="date" value="ASC" /> Date: Ascending
-			</label>
-			<label>
-			<input type="radio" name="date" value="DESC" selected="selected" /> Date: Descending
-			</label>
-			<button>Apply filter</button>
-			<input type="hidden" name="action" value="myfilter">
-			</form>
-
-			</div>
-		</div>
-	</div>
-
 		<div class="row">
 			<div class="col-lg-12">
-				<section id="response" class="blog-articles">
+				<div class="blog-search-filter">
+
+
+					<form action="<?php echo site_url() ?>/wp-admin/admin-ajax.php" method="POST" id="filter">
+						<?php
+						if( $terms = get_terms( array( 'taxonomy' => 'category', 'orderby' => 'name' ) ) ) : 
+
+							echo '<select name="categoryfilter"><option value="">Select category...</option>';
+							foreach ( $terms as $term ) :
+			echo '<option value="' . $term->term_id . '">' . $term->name . '</option>'; // ID of the category as the value of an option
+		endforeach;
+		echo '</select>';
+	endif;
+	?>
+
+
+	<label>
+		<input type="radio" name="date" value="ASC" /> Date: Ascending
+	</label>
+	<label>
+		<input type="radio" name="date" value="DESC" selected="selected" /> Date: Descending
+	</label>
+	<button>Apply filter</button>
+	<input type="hidden" name="action" value="myfilter">
+</form>
+
+</div>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-12">
+		<section id="response" class="blog-articles">
 
 			<?php
-				if(have_posts()) : 
+			if(have_posts()) : 
 				while(have_posts()) : 
-				the_post(); 
+					the_post(); 
 			?>
-			<article class="post">
-				<header>
-					<h2><?php the_title() ?></h2>
-					<div class="tn" style="background-image:ur() no-repeat;">
-						<a href="<?php the_permalink(); ?>" class="blog-tn" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>');"></a>
-					</div>
-					<div class="entry-meta">
-					<?php
-					playground_posted_on();
-					?>
-					</div>
-
-				</header>
-			</article>
-
-				
+			<?php get_template_part( 'template-parts/post-card'); ?>
 
 			<?php
-				endwhile;
-				else : 
+			endwhile;
+			else : 
 			?>
-
 
 			Oops, there are no posts.
 
@@ -88,10 +72,8 @@ get_header();
 			endif;
 			?>
 			
-
-
-			</section>
-</div>		
+		</section>
+	</div>		
 </div>
 
 </main><!-- #main -->
