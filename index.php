85475d1c793ff43c1962b93883da56eadef0c582
Terry Fadz <terry@glantz.net>
2019-10-22 07:08:41 -0500

    updates to post card

--- index.php
+++ index.php
@@ -19,61 +19,33 @@ get_header();
 	<main id="main" class="site-main container">
 
 		<div class="row">
-			<div class="col-lg-12">
-				<div class="blog-search-filter">
-
-					<form action="<?php echo site_url() ?>/wp-admin/admin-ajax.php" method="POST" id="filter">
-						<?php
-						if( $terms = get_terms( array( 'taxonomy' => 'category', 'orderby' => 'name' ) ) ) : 
-
-							echo '<select name="categoryfilter" class="select-css"><option value="">Select category</option>';
-							foreach ( $terms as $term ) :
-							echo '<option value="' . $term->term_id . '">' . $term->name . '</option>'; // ID of the category as the value of an option
-							endforeach;
-						echo '</select>';
-						endif;
-						?>
-						<label>
-							<input type="radio" name="date" value="ASC" /> Date: Ascending
-						</label>
-						<label>
-							<input type="radio" name="date" value="DESC" selected="selected" /> Date: Descending
-						</label>
-
-						<div class="filter-actions">
-							<button id="filter-btn" disabled>Apply filter</button>
-							<button id="reset-btn" disabled>Reset</button>
-							<input type="hidden" name="action" value="myfilter">
-						</div>
-					</form>
-				</div>
+			<div class="col-lg-4">
+						<?php echo do_shortcode( '[searchandfilter fields="search,category,post_tag" types=",checkbox,checkbox" headings=",Categories,Tags"]
+' ); ?>
 			</div>
-		</div>
 
-		<div class="row">
-			<div class="col-lg-12">
-				<section id="response" class="blog-articles">
 
-					<?php
-						if(have_posts()) : 
-						while(have_posts()) : the_post(); 
+		<div class="col-lg-8">
+			<?php
+			if(have_posts()) : 
+				while(have_posts()) : the_post(); 
 					?>
 					<?php get_template_part( 'template-parts/post-card'); ?>
-
-					<?php endwhile; else : ?>
-
-					Oops, there are no posts.
-
-					<?php
-					endif;
-					?>
-					
-				</section>
-			</div>		
+				<?php endwhile; else : ?>
+				Oops, there are no posts.
+				<?php
+			endif;
+			?>
 		</div>
+	</main>
+		
+
+
+		
+
 
 </main><!-- #main -->
 </div><!-- #primary -->
 
 <?php
-get_footer();
+// get_footer();
